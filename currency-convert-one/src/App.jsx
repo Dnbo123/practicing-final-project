74aed89bfad4912cbb4c19e72f05dd9efb4c1fd0
Don Bosco <don965877@gmail.com>
2024-09-28 07:43:46 +0300

    transferred local practice directory to repo

---
+++ currency-convert-one/src/App.jsx
@@ -0,0 +1,15 @@
+import ConvertForm from "./components/convertForm.jsx"
+
+const App = () => {
+  return (
+    <div className="currency-converter">
+       <h2 className="converter-header"> Currency converter App</h2>
+    <ConvertForm />
+      </div>
+     
+        
+    
+  )
+}
+
+export default App
